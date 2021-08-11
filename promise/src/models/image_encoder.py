b1f61eb0358184d0a82fdaf5a26d4580180ea180
1maginat0r <imaginai90@proton.me>
2021-08-11 11:07:38 +0000

    test docs

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -14,4 +14,21 @@ class Adapter(nn.Module):
     ):
         super().__init__()
         self.linear1 = nn.Linear(input_dim, mid_dim)
-        s
+        self.conv = nn.Conv3d(in_channels = mid_dim, out_channels = mid_dim, kernel_size=3, padding=1, groups=mid_dim)
+        self.linear2 = nn.Linear(mid_dim, input_dim)
+
+    def forward(self, features):
+        out = self.linear1(features)
+        out = F.relu(out)
+        out = out.permute(0, 4, 1, 2, 3)
+        out = self.conv(out)
+        out = out.permute(0, 2, 3, 4, 1)
+        out = F.relu(out)
+        out = self.linear2(out)
+        out = F.relu(out)
+        out = features + out
+        return out
+
+class LayerNorm3d(nn.Module):
+    def __init__(self, num_channels: int, eps: float = 1e-6) -> None:
+        supe
