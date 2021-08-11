e8e977c80e61e176d2f8b676789020aabb860bf9
1maginat0r <imaginai90@proton.me>
2021-08-11 11:21:08 +0000

    Update from Forestry.io - about.md

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -31,4 +31,22 @@ class Adapter(nn.Module):
 
 class LayerNorm3d(nn.Module):
     def __init__(self, num_channels: int, eps: float = 1e-6) -> None:
-        supe
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(num_channels))
+        self.bias = nn.Parameter(torch.zeros(num_channels))
+        self.eps = eps
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        u = x.mean(1, keepdim=True)
+        s = (x - u).pow(2).mean(1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.eps)
+        x = self.weight[:, None, None, None] * x + self.bias[:, None, None, None]
+        return x
+
+
+def positionalencoding1d(d_model, length):
+    """
+    :param d_model: dimension of the model
+    :param length: length of positions
+    :return: length*d_model position matrix
+    ""
