07434aa9d4009ccd3a92d0d8a4e47cbae3a307d6
1maginat0r <imaginai90@proton.me>
2021-08-11 12:24:41 +0000

    remove require

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -49,4 +49,20 @@ def positionalencoding1d(d_model, length):
     :param d_model: dimension of the model
     :param length: length of positions
     :return: length*d_model position matrix
-    ""
+    """
+    if d_model % 2 != 0:
+        raise ValueError("Cannot use sin/cos positional encoding with "
+                         "odd dim (got dim={:d})".format(d_model))
+    pe = torch.zeros(length, d_model)
+    position = torch.arange(0, length).unsqueeze(1)
+    div_term = torch.exp((torch.arange(0, d_model, 2, dtype=torch.float) *
+                         -(math.log(10000.0) / d_model)))
+    pe[:, 0::2] = torch.sin(position.float() * div_term)
+    pe[:, 1::2] = torch.cos(position.float() * div_term)
+
+    return pe
+
+def positionalencoding2d(d_model, height, width):
+    """
+    :param d_model: dimension of the model
+ 
