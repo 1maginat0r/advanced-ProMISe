4159555cfa0ea21a02e78329283ca8b610c679a1
1maginat0r <imaginai90@proton.me>
2021-08-11 17:54:39 +0000

    add README.md and LICENSE

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -78,4 +78,18 @@ def positionalencoding2d(d_model, height, width):
     div_term = torch.exp(torch.arange(0., d_model, 2) *
                          -(math.log(10000.0) / d_model))
     pos_w = torch.arange(0., width).unsqueeze(1)
-    pos
+    pos_h = torch.arange(0., height).unsqueeze(1)
+    pe[0:d_model:2, :, :] = torch.sin(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    pe[1:d_model:2, :, :] = torch.cos(pos_w * div_term).transpose(0, 1).unsqueeze(1).repeat(1, height, 1)
+    pe[d_model::2, :, :] = torch.sin(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+    pe[d_model + 1::2, :, :] = torch.cos(pos_h * div_term).transpose(0, 1).unsqueeze(2).repeat(1, 1, width)
+
+    return pe
+
+
+class Promise(nn.Module):
+    def __init__(
+            self,
+            img_size: int = 1024,
+            patch_size: int = 16,
+  
