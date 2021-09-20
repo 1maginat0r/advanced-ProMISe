6a62e79dc89e4e8e95e693a5623d8c685155f348
1maginat0r <imaginai90@proton.me>
2021-09-20 18:22:39 +0000

    Update composer packages

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -432,4 +432,18 @@ def window_partition(x: torch.Tensor, window_size: int) -> Tuple[torch.Tensor, T
 
 
 def window_unpartition(
-        windows: torch
+        windows: torch.Tensor, window_size: int, pad_hw: Tuple[int, int, int], hw: Tuple[int, int, int]
+) -> torch.Tensor:
+    """
+    Window unpartition into original sequences and removing padding.
+    Args:
+        windows (tensor): input tokens with [B * num_windows, window_size, window_size, C].
+        window_size (int): window size.
+        pad_hw (Tuple): padded height and width (Hp, Wp).
+        hw (Tuple): original height and width (H, W) before padding.
+    Returns:
+        x: unpartitioned sequences with [B, H, W, C].
+    """
+    Hp, Wp, Dp = pad_hw
+    H, W, D = hw
+    B = windows.shape[0] // (Hp * Wp * Dp // window_size // window_
