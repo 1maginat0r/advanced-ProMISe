c62f488a5c8a9d0c6e03bcec7b44cdba29fd27e0
1maginat0r <imaginai90@proton.me>
2021-09-23 15:35:47 +0000

    Fix android build

--- promise/src/models/image_encoder.py
+++ promise/src/models/image_encoder.py
@@ -462,4 +462,20 @@ def get_rel_pos(q_size: int, k_size: int, rel_pos: torch.Tensor) -> torch.Tensor
         query and key sizes.
     Args:
         q_size (int): size of query q.
-        k_size (int): size of key k
+        k_size (int): size of key k.
+        rel_pos (Tensor): relative position embeddings (L, C).
+    Returns:
+        Extracted positional embeddings according to relative positions.
+    """
+    max_rel_dist = int(2 * max(q_size, k_size) - 1)
+    # Interpolate rel pos if needed.
+    if rel_pos.shape[0] != max_rel_dist:
+        # Interpolate rel pos.
+        rel_pos_resized = F.interpolate(
+            rel_pos.reshape(1, rel_pos.shape[0], -1).permute(0, 2, 1),
+            size=max_rel_dist,
+            mode="linear",
+        )
+        rel_pos_resized = rel_pos_resized.reshape(-1, max_rel_dist).permute(1, 0)
+    else:
+ 
